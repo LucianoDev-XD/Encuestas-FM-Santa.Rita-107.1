fc5246335390abe46b497f29a8474732d1b98bc5
LucianoDev-XD <alejofabi96@gmail.com>
2025-10-08 00:34:05 -0300

    cambios leves

--- src/services/Firebase.js
+++ src/services/Firebase.js
@@ -4,23 +4,23 @@ import { getAuth } from "firebase/auth"
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAyCfabspsrOoiYkzi6X-kr_8YHxlEzATY",
-  authDomain: "encuestas-esquina.firebaseapp.com",
-  projectId: "encuestas-esquina",
-  storageBucket: "encuestas-esquina.appspot.com",
-  messagingSenderId: "132414285181",
-  appId: "1:132414285181:web:9aa76bccca64af1fb05814"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
 
 // App Check con reCAPTCHA v3
-// if (import.meta.env.DEV) {
-//   self.FIREBASE_APPCHECK_DEBUG_TOKEN = true
-// }
+if (import.meta.env.DEV) {
+  self.FIREBASE_APPCHECK_DEBUG_TOKEN = true
+}
 
 initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider("6Lfy5eArAAAAAJg28yKDk6HMZnkdXxmLxu5zonMP"),
+  provider: new ReCaptchaV3Provider("import.meta.env.VITE_RECAPTCHA_KEY"),
   isTokenAutoRefreshEnabled: true
 })
 
